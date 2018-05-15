6ba85671cded20ae6a093f3187eab7690ef8e284
ShrutiSoni1991 <shruti9575@gmail.com>
2018-05-15 06:26:09 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, trigger } from '@angular/core';
 import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'app';
+  constructor(private dataService:DataService){
+
+
+  }
+  someProperty:String = '' ;
+
+  ngOnOnint(){
+    console.log(this.dataService.cars);
+  this.someProperty = this.dataService.myData();
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DataService } from './data.service';
-
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { importType } from '@angular/compiler/src/output/output_ast';
+import {trigger, state, style, transition, animation, keyframes} from '@angular/animations';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [ DataService ],
   bootstrap: [AppComponent]
